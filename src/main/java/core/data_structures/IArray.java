7b2dbcba13bf85bd22df03c5eef71f0718d3f1f1
Javier Cabrera <javierca@kth.se>
2019-08-06 11:57:25 +0200

    STRAC core

---
+++ src/main/java/core/data_structures/IArray.java
@@ -0,0 +1,7 @@
+package core.data_structures;
+
+public interface IArray<T> extends IReadArray<T> {
+
+    void set(long position, T value);
+
+}
---
+++ src/main/java/core/utils/SetHelper.java
@@ -0,0 +1,10 @@
+package core.utils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetHelper {
+
+
+
+}
